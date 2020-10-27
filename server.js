077d9f7d9dc930d2327fa4ca80fa7fae5ed897f2
Edvard <45853648+EdvardSire@users.noreply.github.com>
2020-10-27 19:44:55 +0100

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+//Basic Node server
+const http = require("http");
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.writeHead(200); // status code
+
+    let content = {
+        "id":123
+    };
+    let data = JSON.stringify(content);
+
+    res.end(data);
+});
+
+server.listen(port, () => {
+    console.log("LIstening on port")
+})
