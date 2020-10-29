65b442ea01a52e86aac56a87adc45d8ec6e164eb
Edvard <45853648+EdvardSire@users.noreply.github.com>
2020-10-29 18:53:49 +0100

    better reqest and response

--- server.js
+++ server.js
@@ -2,26 +2,39 @@
 const fs = require("fs");
 
 getJSON = (filename) => {
-    return fs.existsSync(filename)
-    ? JSON.parse(fs.readFileSync(filename).toString())
-    : "Error";
+	return fs.existsSync(filename)
+		? JSON.parse(fs.readFileSync(filename).toString())
+		: "Wrong filename";
 };
-
 
 //Basic Node server
 const http = require("http");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.writeHead(200); // status code
+	// Handle request
+	if (req.headers["content-type"] != "text/plain") {
+		res.statusCode = 400;
+		res.end();
+	}
 
-    let data = JSON.stringify(getJSON("dat.json"));
+	let data = "";
+	req.on("data", (chunk) => {
+		data += chunk.toString("utf8");
+	});
 
-    res.end(data);
+	req.on("end", () => {
+		//Create response
+		res.setHeader("Content-type", "application/json");
+		res.setHeader("Access-Control-Allow-Origin", "*");
+
+		res.statusCode = 200;
+		res.end();
+		console.log(data);
+		//JSON.stringify(getJSON("data.json"))
+	});
 });
 
 server.listen(port, () => {
-    console.log("LIstening on port");
+	console.log("Listening on port: " + port);
 });
