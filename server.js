ed567a2dbb98f7ab5e8e440c17b2c2af16669b15
Edvard <45853648+EdvardSire@users.noreply.github.com>
2020-11-02 15:02:13 +0100

    refractoring, displayHtml()

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-//JSON function
+//Imports
+const http = require("http");
 const fs = require("fs");
 
+//JSON function
 getJSON = (filename) => {
 	return fs.existsSync(filename)
 		? JSON.parse(fs.readFileSync(filename).toString())
@@ -8,24 +10,40 @@ getJSON = (filename) => {
 };
 
 //Basic Node server
-const http = require("http");
 const port = process.env.PORT || 3000;
-
 const server = http.createServer((req, res) => {
+	//Functions
+	displayHtml = (filename) => {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		fs.readFile(filename, "utf8", (err, data) => {
+			res.write(data);
+			res.end();
+		});
+	};
+
 	// GET or POST
 	switch (req.method) {
 		case "GET":
-			res.statusCode = 200;
-			res.end();
+			switch (req.url) {
+				case "/":
+					displayHtml("./index.html");
+					break;
+				case "/api/get":
+					break;
+				default:
+					res.statusCode = 405;
+					res.end();
+			}
+			break;
 
 		case "POST":
 			res.statusCode = 201;
 			res.end();
+			break;
 
 		default:
-			response.writeHeader(200, { "Content-Type": "text/html" });
-			response.write(html);
-			response.end();
+			res.statusCode = 405;
+			res.end();
 	}
 });
 
